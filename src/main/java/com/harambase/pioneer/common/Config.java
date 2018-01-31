80cbdea684df6105fd541124ea59e65e4836d5cb
Shilei Lin <lin.shilei@outlook.com>
2018-01-31 14:17:54 +0800

    修改Config类

--- src/main/java/com/harambase/pioneer/common/Config.java
+++ src/main/java/com/harambase/pioneer/common/Config.java
@@ -1,13 +1,22 @@
 package com.harambase.pioneer.common;
 
+import org.springframework.util.ResourceUtils;
+
 import java.util.ResourceBundle;
 
 public class Config {
 
+    public static String serverPath = "";
     private static ResourceBundle resourceBundle = ResourceBundle.getBundle("server");
-
     public final static String TEMP_FILE_PATH = resourceBundle.getString("tempFile.path");
     public final static String SERVER_IP = resourceBundle.getString("server.ip");
     public final static int SERVER_PORT = Integer.parseInt(resourceBundle.getString("server.port"));
 
+    static {
+        try {
+            serverPath = ResourceUtils.getURL("classpath:").getPath();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
