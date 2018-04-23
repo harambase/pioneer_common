ce4db43f42b5bf162cb4ac46fac1580ece9e7c74
Shilei Lin <lin.shilei@outlook.com>
2018-04-23 23:38:59 +0800

    添加信息TAGs和LABELs

---
+++ src/main/java/com/harambase/pioneer/common/MessageLabels.java
@@ -0,0 +1,7 @@
+package com.harambase.pioneer.common;
+
+public class MessageLabels {
+    public final static String IMPORTANT = "重要";
+    public final static String NORMAL = "一般";
+    public final static String URGENT = "紧急";
+}
---
+++ src/main/java/com/harambase/pioneer/common/MessageTags.java
@@ -0,0 +1,9 @@
+package com.harambase.pioneer.common;
+
+public class MessageTags {
+    public final static String REGULAR = "普通";
+    public final static String COURSE = "教学";
+    public final static String TEACH = "教务";
+    public final static String STUDENT = "学生";
+    public final static String SYSTEM = "系统";
+}
