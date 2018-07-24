5bc57eb70e41e338aa1e063fec0379e66a2c682b
s1lin <lin.shilei@outlook.com>
2018-07-24 22:34:09 +0800

    FTP 上传、下载、删除。

--- src/main/java/com/harambase/pioneer/common/Config.java
+++ src/main/java/com/harambase/pioneer/common/Config.java
@@ -9,9 +9,11 @@ public class Config {
     public static String serverPath = "";
     private static ResourceBundle resourceBundle = ResourceBundle.getBundle("server");
     public final static String TEMP_FILE_PATH = resourceBundle.getString("tempFile.path");
+
     public final static String SERVER_IP = resourceBundle.getString("server.ip");
     public final static int SERVER_PORT = Integer.parseInt(resourceBundle.getString("server.port"));
 
+    public final static String FTP_PATH = resourceBundle.getString("ftp.path");
     public final static String FTP_SERVER   = resourceBundle.getString("ftp.server");
     public final static String FTP_USERNAME = resourceBundle.getString("ftp.user");
     public final static String FTP_PASSWORD = resourceBundle.getString("ftp.password");
--- src/main/java/com/harambase/pioneer/common/support/util/FileUtil.java
+++ src/main/java/com/harambase/pioneer/common/support/util/FileUtil.java
@@ -1,15 +1,21 @@
 package com.harambase.pioneer.common.support.util;
 
 import com.harambase.pioneer.common.Config;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPReply;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
-import java.util.logging.Logger;
 
 public class FileUtil {
+
+    private final static Logger logger = LoggerFactory.getLogger("FileUtil");
 
     @Deprecated
     public static void downloadFile(String fileName, String logicalPath, HttpServletResponse response) throws Exception {
@@ -56,107 +62,193 @@ public class FileUtil {
         return imgPath;
     }
 
-    public static String uploadFileToFtpServer(MultipartFile file, String server, String username, String password) {
+    public static void downloadFileFromFtpServer(HttpServletResponse response, String fileName, String filePath, String server, String username, String password) throws Exception {
 
-        String fileName = file.getName();
-        FTPClient ftpClient = new FTPClient();
-
-        try {
-            int reply;
-
-            ftpClient.connect(server);
-            ftpClient.login(username, password);
-
-            System.out.println("Connected to " + server + ".");
-            System.out.print(ftpClient.getReplyString());
-
-            // After connection attempt, you should check the reply code to verify
-            // success.
-            reply = ftpClient.getReplyCode();
-
-            if (!FTPReply.isPositiveCompletion(reply)) {
-                ftpClient.disconnect();
-                System.err.println("FTP server refused connection.");
-                return null;
-            }
-
-            InputStream fis = file.getInputStream();
-            ftpClient.storeFile(fileName, fis);
-
-            ftpClient.logout();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (ftpClient.isConnected()) {
-                try {
-                    ftpClient.disconnect();
-                } catch (IOException ioe) {
-                    // do nothing
-                }
-            }
-
-        }
-        return fileName;
-    }
-
-    public static void downloadFileFromFtpServer(String fileName, String logicalPath,
-                                                 String server, String username, String password,
-                                                 HttpServletResponse response) throws Exception {
+        byte[] bytes = fileByteFromFtp(filePath, server, username, password);
         fileName = new String(fileName.getBytes(), "ISO-8859-1");
-        String filePath = Config.TEMP_FILE_PATH + logicalPath;
-        OutputStream outputStream = new BufferedOutputStream(response.getOutputStream());
-        FTPClient ftpClient = new FTPClient();
-
-        try {
-            int reply;
-
-            ftpClient.connect(server);
-            ftpClient.login(username, password);
-
-            System.out.println("Connected to " + server + ".");
-            System.out.print(ftpClient.getReplyString());
-
-            // After connection attempt, you should check the reply code to verify
-            // success.
-            reply = ftpClient.getReplyCode();
-
-            if (!FTPReply.isPositiveCompletion(reply)) {
-                ftpClient.disconnect();
-                System.err.println("FTP server refused connection.");
-                return;
-            }
-
-            ftpClient.retrieveFile(filePath, outputStream);
-            ftpClient.logout();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (ftpClient.isConnected()) {
-                try {
-                    ftpClient.disconnect();
-                } catch (IOException ioe) {
-                    // do nothing
-                }
-            }
-
-        }
 
         response.reset();
         response.setHeader("Content-Disposition", "attachment; filename=\"" + fileName);
         response.setContentType("application/octet-stream;charset=UTF-8");
         response.setCharacterEncoding("UTF-8");
-
-        FileInputStream fileInputStream = new FileInputStream(filePath);
-
-        byte[] bytes = new byte[2048];
-        int length;
-        while ((length = fileInputStream.read(bytes)) > 0) {
-            outputStream.write(bytes, 0, length);
-        }
-        fileInputStream.close();
-        outputStream.flush();
-        outputStream.close();
+        ServletOutputStream out = response.getOutputStream();
+        out.write(bytes);
+        out.flush();
+        out.close();
     }
+
+    public static String uploadFileToFtpServer(MultipartFile file, String dir, String server, String username, String password) {
+
+        String fileName = file.getOriginalFilename();
+        String dirName = FileWriterUtil.getSecondPathByHashCode(dir, fileName);
+        String filePath = FileWriterUtil.getSingleFileDirName(fileName, dirName);
+        String fileLogicalName = getFileLogicalName(filePath);
+
+        FTPClient ftpClient = new FTPClient();
+
+        try {
+            int reply;
+
+            ftpClient.connect(server);
+            ftpClient.login(username, password);
+
+            /*
+             二进制方式，首先二进制方式保证了文件内容所有数据位都是重要的。经过验证在传输文件类型为非文本内容的文件，使用ASCII传输方式造成copy的文件已经损坏。
+             */
+            ftpClient.setFileType(FTP.BINARY_FILE_TYPE); //大坑！！！！！
+
+            logger.info("FTP: Connected to " + server + ".");
+            logger.info("FTP: " + ftpClient.getReplyString());
+
+            reply = ftpClient.getReplyCode();
+
+            //Success: reply >= 200 && reply < 300;
+            if (!FTPReply.isPositiveCompletion(reply)) {
+                ftpClient.disconnect();
+                logger.error("FTP: FTP server refused connection.");
+                return null;
+            }
+
+            ftpClient.makeDirectory(dirName);
+            ftpClient.changeWorkingDirectory(dirName);
+            InputStream fis = file.getInputStream();
+            ftpClient.storeFile(fileLogicalName, fis);
+
+            ftpClient.logout();
+
+            return filePath;
+
+        } catch (IOException e) {
+            logger.error(e.getMessage(), e);
+            return null;
+
+        } finally {
+            if (ftpClient.isConnected()) {
+                try {
+                    ftpClient.disconnect();
+                } catch (IOException ioe) {
+                    // do nothing
+                }
+            }
+
+        }
+
+    }
+
+    public static boolean deleteFileFromFTP(String filePath, String server, String username, String password) {
+
+        FTPClient ftpClient = new FTPClient();
+        boolean error;
+
+        try {
+            int reply;
+
+            ftpClient.connect(server);
+            ftpClient.login(username, password);
+
+            logger.info("FTP: Connected to " + server + ".");
+            logger.info("FTP: " + ftpClient.getReplyString());
+
+            reply = ftpClient.getReplyCode();
+
+            //Success: reply >= 200 && reply < 300;
+            if (!FTPReply.isPositiveCompletion(reply)) {
+                ftpClient.disconnect();
+                logger.error("FTP: FTP server refused connection.");
+                return false;
+            }
+
+            error = ftpClient.deleteFile(filePath);
+            if (error) {
+                logger.error("FTP: FTP file deletion failed. File path: " + filePath);
+                return false;
+            }
+
+            ftpClient.logout();
+            return true;
+
+        } catch (IOException e) {
+            logger.error(e.getMessage(), e);
+            return false;
+        } finally {
+            if (ftpClient.isConnected()) {
+                try {
+                    ftpClient.disconnect();
+                } catch (IOException ioe) {
+                    // do nothing
+                }
+            }
+
+        }
+
+    }
+
+    private static String getFileLogicalName(String filePath) {
+        String[] paths = filePath.split("/");
+        return paths[paths.length - 1];
+    }
+
+    private static String getFileDirPath(String filePath) {
+        String[] paths = filePath.split("/");
+        String dirPath = "/";
+        for (int i = 1; i < paths.length - 1; i++) {
+            dirPath += paths[i] + "/";
+        }
+        return dirPath;
+    }
+
+    private static byte[] fileByteFromFtp(String filePath, String server, String username, String password) {
+
+        byte[] bytes = {};
+        FTPClient ftpClient = new FTPClient();
+
+        try {
+            int reply;
+
+            ftpClient.connect(server);
+            ftpClient.login(username, password);
+            ftpClient.setControlEncoding("UTF-8");
+            ftpClient.setBufferSize(1024);
+
+            logger.info("FTP: Connected to " + server + ".");
+            logger.info("FTP: " + ftpClient.getReplyString());
+
+            // After connection attempt, you should check the reply code to verify
+            // success.
+            reply = ftpClient.getReplyCode();
+
+            if (!FTPReply.isPositiveCompletion(reply)) {
+                ftpClient.disconnect();
+                logger.error("FTP: FTP server refused connection.");
+                return bytes;
+            }
+
+            String fileLogicalName = getFileLogicalName(filePath);
+            String dirPath = getFileDirPath(filePath);
+            ftpClient.changeWorkingDirectory(dirPath);
+            logger.info("FTP: FTP current working directory = " + ftpClient.printWorkingDirectory());
+
+            InputStream ftpInputStream = ftpClient.retrieveFileStream(fileLogicalName);
+            bytes = IOUtils.toByteArray(ftpInputStream);
+
+            ftpInputStream.close();
+            ftpClient.logout();
+
+            return bytes;
+
+        } catch (IOException e) {
+            logger.error(e.getMessage(), e);
+            return bytes;
+        } finally {
+            if (ftpClient.isConnected()) {
+                try {
+                    ftpClient.disconnect();
+                } catch (IOException ioe) {
+                    // do nothing
+                }
+            }
+
+        }
+    }
+
 }
