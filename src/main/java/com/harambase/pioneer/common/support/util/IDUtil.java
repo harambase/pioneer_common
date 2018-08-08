3ee95db27d9ab95b52602109cd69dd4f9baa206b
linsh <lin.shilei@outlook.com>
2018-08-08 21:58:50 +0800

    添加生成合同ID

--- src/main/java/com/harambase/pioneer/common/support/util/IDUtil.java
+++ src/main/java/com/harambase/pioneer/common/support/util/IDUtil.java
@@ -23,4 +23,9 @@ public class IDUtil {
         Integer last = (int) (Math.random() * (999 - 100 + 1) + 100);
         return "8" + info.split("-")[0] + info.split("-")[1] + last;
     }
+
+    public static String genContractID(String initDate) {
+        Integer last = (int) (Math.random() * (9999 - 1000 + 1) + 1000);
+        return initDate.split("-")[0] + initDate.split("-")[1] + initDate.split("-")[2] + last;
+    }
 }
